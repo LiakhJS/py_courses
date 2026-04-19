620042e97b5b6d13f1dfecfc86cdb1ff05b0b9ed
Nastya <anastasialiakh@hotmail.com>
2026-04-19 21:26:51 +0300

    adding second time L8

---
+++ Lecture_8_Liakh_Nastya/even/tests_is_even.py
@@ -0,0 +1,38 @@
+# import is_even as m
+
+
+# # m.is_even
+# def test_is_even_40_true():
+#     assert m.is_even(40) == True
+
+
+# def test_is_even_1_false():
+#     assert m.is_even(1) == False
+
+
+# def test_is_neg_1_false():
+#     assert m.is_even(-1) == False
+
+# def test_is_neg_8_true():
+#     assert m.is_even(8) == True
+
+
+# # m.my_pow
+# def test_my_pow_4_true():
+#     assert m.my_pow(4) == 16
+
+
+# def test_my_pow_3_true():
+#     assert m.my_pow(3) == 9
+
+
+# def test_my_pow_1_true():
+#     assert m.my_pow(1) == 1
+
+
+# # m.nameUpper
+# def test_nameUpper_steve():
+#     assert m.nameUpper('steve') == 'STEVE'
+
+# # python -m pytest tests_is_even.py имя_файла_внутри_папки_без_точек
+
