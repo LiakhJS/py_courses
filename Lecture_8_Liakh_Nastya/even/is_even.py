dd40436e3f9154a08a5113f9ee351fe5eb64d106
Nastya <anastasialiakh@hotmail.com>
2026-04-19 21:25:28 +0300

    deleting L8

--- Lecture_8_Liakh_Nastya/even/is_even.py
+++
@@ -1,9 +0,0 @@
-# def is_even(num):
-#     return num % 2 == 0
-        
-# def my_pow(num):
-#     return num**2
-
-# def nameUpper(name:str):
-#     return name.upper()
-
--- Lecture_8_Liakh_Nastya/units pytestcov example/main.py
+++
@@ -1,8 +0,0 @@
-# def howmanyletters(input: str):
-#     if len(input) < 1:
-#         return "no data"
-#     elif len(input) <3:
-#         return "less than three letters!?"
-    
-#     return str(input).split(' ')
-
